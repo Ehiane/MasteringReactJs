8d42700f63ef3c8683666c2795afdaece578eb4d
Ehiane <e.oigiagbe@wsu.edu>
2024-01-16 17:54:18 -0800

    adding nested elements in react

--- firstReact/index.js
+++ firstReact/index.js
@@ -1,1 +1,9 @@
-ReactDOM.render(<h1>Hello, Everyone!</h1>,document.getElementById('root'))
+// how to write smth to a div in react
+//# you can only render one thing at a time.
+// ReactDOM.render(<h1>React List</h1>,document.getElementById('root')) //content, position
+
+//# adding nested elements in REACT 
+ReactDOM.render(<ul>
+    <li>a</li>
+    <li>ab</li>
+</ul>, document.getElementById('root'))
